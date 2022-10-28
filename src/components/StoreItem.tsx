e12186069946c874c40c2fa922f340162843ee51
Goga <gogaimerlishvili1@gmail.com>
2022-10-28 18:12:15 +0400

    added store item

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type StoreItemProps = {
+    id: number,
+    name: string,
+    price: number,
+    imgUrl: string
+}
+
+const StoreItem = ({id,name,price,imgUrl}: StoreItemProps) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StoreItem
