e9228432eb6cfbfeb3e53d05f475539d95a9ae09
Goga <gogaimerlishvili1@gmail.com>
2022-11-21 15:04:21 +0400

    added main context for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import {useState} from 'react'
+import HangmanDrawing from '../components/HangmanDrawing'
+import HangmanWord from '../components/HangmanWord'
+import Keyboard from '../components/Keyboard'
+import words from "../data/wordList.json"
+
 
 const Home = () => {
+  const [wordToGuess,setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+
   return (
-    <div>Home</div>
+    <div
+    className="main"
+    style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "-15px -250px",
+      alignItems: "center",
+    }}
+    >
+      <div style={{fontSize: "2rem",textAlign: "center"}}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord /> 
+      <div className='keyboard' style={{alignSelf:"stretch"}}>
+      <Keyboard />
+      </div>
+    </div>
   )
 }
 
