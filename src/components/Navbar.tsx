0b36841740f91d88ff669ff7435ad15904b20f57
Goga <gogaimerlishvili1@gmail.com>
2022-11-21 19:19:00 +0400

    change navbar name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
     <Container>
         <Nav className="me-auto">
-            <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+            <Nav.Link to="/" as={NavLink}>MiniGame</Nav.Link>
             <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
             <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
         </Nav>
