d3b301621c4eff1cbc5d9e8755755c1dd48e2dfb
Goga <gogaimerlishvili1@gmail.com>
2022-11-23 17:09:58 +0400

    add options and logic

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {useState} from "react"
+import Select, { SelectOptions } from "../components/Select"
+
+const options = [
+  {label: "First",value: 1},
+  {label: "Second", value:2 },
+  {label: "Third", value:3 },
+  {label: "Fourth", value:4 },
+  {label: "Fifth", value:5 },
+
+]
 
 const About = () => {
+  const [value1,setValue1] = useState<SelectOptions[]>([options[0]])
+  const [value2,setValue2] = useState<SelectOptions | undefined>(options[0])
+
   return (
-    <div>About</div>
+    <>
+      <Select multiple options={options} value={value1} onChange={o => setValue1(o)} />
+      <br />
+      <Select  options={options} value={value2} onChange={o => setValue2(o)} />
+    </>
   )
 }
 
-export default About
+export default About;
