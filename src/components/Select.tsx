c7453268bab8259b00c7bdbd73899559d8ab0ba0
Goga <gogaimerlishvili1@gmail.com>
2022-11-23 17:37:13 +0400

    add ref

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react"
+import {useEffect, useState,useRef} from "react"
 import styles from "./select.module.css"
 
 export type SelectOptions = {
@@ -26,13 +26,14 @@ type SelectProps = {
 const Select = ({multiple,value,onChange,options}: SelectProps) => {
   const [isOpen,setIsOpen] = useState(false)
   const [highlightedIndex,setHighlightedIndex] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null)
 
   const clearOptions = () => {
    multiple ? onChange([]) : onChange(undefined)
   }
 
   const selectOption = (option: SelectOptions) =>{
-    console.log(option)
+  
     if(multiple){
       if(value.includes(option)){
         onChange(value.filter(o => o !== option))
@@ -52,9 +53,44 @@ const Select = ({multiple,value,onChange,options}: SelectProps) => {
   useEffect(() => {
     if(isOpen) setHighlightedIndex(0)
   },[isOpen])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if(e.target !== containerRef.current) return
+
+      switch(e.code){
+        case "Enter":
+          case "Space":
+            setIsOpen(prev => !prev)
+            if(isOpen) selectOption(options[highlightedIndex])
+            break
+            case "ArrowUp":
+              case "ArrowDown" : {
+                if(!isOpen){
+                  setIsOpen(true)
+                  break
+                }
+
+                const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1)
+                if(newValue >= 0 && newValue < options.length){
+                  setHighlightedIndex(newValue)
+                }
+                break
+              }
+                case "Escape" : 
+                setIsOpen(false)
+                break
+      }
+    }
+    containerRef.current?.addEventListener("keydown", handler)
+
+    return () => {
+      containerRef.current?.removeEventListener("keydown",handler)
+    }
+  }, [isOpen,highlightedIndex,options])
   return (
     <>
-    <div onBlur={() => setIsOpen(true)} onClick={() => setIsOpen(prev => !prev)} tabIndex={0} className={styles.container}>
+    <div ref={containerRef} onBlur={() => setIsOpen(true)} onClick={() => setIsOpen(prev => !prev)} tabIndex={0} className={styles.container}>
         <span className={styles.value}>{multiple ? value.map(v => (
           <button className={styles["option-badge"]} key={v.value} onClick={e => {
             e.stopPropagation()
