1df6ade1366bed8c2eaece330601df683e56868c
Goga <gogaimerlishvili1@gmail.com>
2022-11-26 18:56:00 +0400

    added some props

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -3,12 +3,15 @@ import {Form,Stack,Row,Col, Button} from "react-bootstrap"
 import { Link } from "react-router-dom"
 import CreatableReactSelect from "react-select/creatable"
 import {NoteData,Tag} from "../App"
+import {v4 as uuidV4} from "uuid"
 
 type NoteFormProps = {
     onSubmit: (data:NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
 }
 
-const NoteForm = ({onSubmit}: NoteFormProps) => {
+const NoteForm = ({onSubmit,onAddTag,availableTags}: NoteFormProps) => {
     const titleRef=useRef<HTMLInputElement>(null)
     const markdownRef=useRef<HTMLTextAreaElement>(null)
     const [selectedTags,setSelectedTags] = useState<Tag[]>([])
@@ -19,7 +22,7 @@ const NoteForm = ({onSubmit}: NoteFormProps) => {
         onSubmit({
             title: titleRef.current!.value,
             markdown: markdownRef.current!.value,
-            tags: []
+            tags: selectedTags
         })
 
     }
@@ -36,7 +39,16 @@ const NoteForm = ({onSubmit}: NoteFormProps) => {
             <Col>
             <Form.Group controlId="tags">
                 <Form.Label>Tags</Form.Label>
-                <CreatableReactSelect value={selectedTags.map(tag => {
+                <CreatableReactSelect
+                onCreateOption={label => {
+                    const newTag = {id: uuidV4(),label}
+                    onAddTag(newTag)
+                    setSelectedTags(prev => [...prev,newTag])
+                }}
+                value={selectedTags.map(tag => {
+                    return {label: tag.label,value: tag.id}
+                })}
+                options={availableTags.map(tag => {
                     return {label: tag.label,value: tag.id}
                 })}
                 onChange={tags => {
