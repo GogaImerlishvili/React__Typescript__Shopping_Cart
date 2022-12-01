1c477784f0740c16aef79946e3d483060896f596
Goga <gogaimerlishvili1@gmail.com>
2022-12-01 22:09:08 +0400

    added store reducer

--- src/hooks/store.ts
+++ src/hooks/store.ts
@@ -1,10 +1,20 @@
-import {configureStore,ThunkAction,Action} from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
+import { authApi } from "../services/authApi";
+import authReducer from "../features/authSlice"
+import {setupListeners} from "@reduxjs/toolkit/query/react"
+import { useDispatch } from "react-redux";
 
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authReducer,
+        [authApi.reducerPath]: authApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware)
 })
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,RootState,unknown,Action<string>> 
+setupListeners(store.dispatch)
+
+export const useAppDispatch: () => AppDispatch = useDispatch
